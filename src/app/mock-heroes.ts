a83fda842118c059c71efb3b4a899bd640112537
Florian Grazina <114757171+Florian-Grazina@users.noreply.github.com>
2023-04-18 18:58:17 +0200

    List heroes

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+    { id: 1, name: 'PainGratos'},
+    { id: 2, name: 'Skarlah'},
+    { id: 3, name: 'Moakane'},
+    { id: 4, name: 'Arth'},
+    { id: 5, name: 'Elder'},
+    { id: 6, name: 'Jarno'},
+    { id: 7, name: 'Blackbull'},
+    { id: 8, name: 'Cross'},
+    { id: 9, name: 'Zenarus'},
+    { id: 10, name: 'Bruno'}
+]
